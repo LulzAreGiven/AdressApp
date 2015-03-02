f39be35733d0abea8cdcc95819c522ec622eec52
Axel Duenninger <axelduenninger@gmail.com>
2015-03-02 18:25:12 +0100

    Controller for PersonOverview.fxml

--- src/adress/view/PersonOverviewController.java
+++ src/adress/view/PersonOverviewController.java
@@ -3,5 +3,49 @@ package adress.view;
 /**
  * Created by EpicLulz on 02.03.2015.
  */
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import adress.MainApp;
+import adress.model.Person;
+
 public class PersonOverviewController {
+    @FXML
+    private TableView<Person> personTable;
+    @FXML
+    private TableColumn<Person, String> firstNameColumn;
+    @FXML
+    private TableColumn<Person, String> lastNameColumn;
+
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+
+    private MainApp mainApp;
+
+    public PersonOverviewController(){
+    }
+
+    @FXML
+    private void initialize(){
+        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+    }
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+        personTable.setItems(mainApp.getPersonData());
+    }
 }
