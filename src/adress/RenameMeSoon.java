4284e7887cfba63fcc8c07d93f2a49021b4b7e11
LulzAreGiven - Axel Dünninger <axelduenninger@gmail.com>
2015-03-02 12:54:49 +0100

    Initial Classes

---
+++ src/adress/RenameMeSoon.java
@@ -0,0 +1,7 @@
+package adress;
+
+/**
+ * Created by A on 02.03.2015.
+ */
+public class RenameMeSoon {
+}
---
+++ src/adress/model/RenameMe.java
@@ -0,0 +1,7 @@
+package adress.model;
+
+/**
+ * Created by A on 02.03.2015.
+ */
+public class RenameMe {
+}
