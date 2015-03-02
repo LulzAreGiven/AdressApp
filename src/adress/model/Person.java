dd079821e751b6e5824e0df1bbf0dd3f0f9071c1
Axel Duenninger <axelduenninger@gmail.com>
2015-03-02 18:57:27 +0100

    PersonDate updates now the Columns

--- src/adress/model/Person.java
+++ src/adress/model/Person.java
@@ -1,12 +1,8 @@
 package adress.model;
-import java.time.LocalDate;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.property.*;
+
+import java.time.LocalDate;
 
 public class Person {
 
@@ -24,7 +20,7 @@ public class Person {
 
     public Person(String firstName, String lastName){
         this.firstName = new SimpleStringProperty(firstName);
-        this.lastName = new SimpleStringProperty((lastName);
+        this.lastName = new SimpleStringProperty((lastName));
 
         this.street = new SimpleStringProperty("Blubstr. 1337");
         this.postalCode = new SimpleIntegerProperty(1234);
@@ -37,71 +33,71 @@ public class Person {
         return firstName.get();
     }
 
-    public StringProperty firstNameProperty() {
-        return firstName;
-    }
-
     public void setFirstName(String firstName) {
         this.firstName.set(firstName);
+    }
+
+    public StringProperty firstNameProperty() {
+        return firstName;
     }
 
     public String getLastName() {
         return lastName.get();
     }
 
-    public StringProperty lastNameProperty() {
-        return lastName;
-    }
-
     public void setLastName(String lastName) {
         this.lastName.set(lastName);
+    }
+
+    public StringProperty lastNameProperty() {
+        return lastName;
     }
 
     public String getStreet() {
         return street.get();
     }
 
-    public StringProperty streetProperty() {
-        return street;
-    }
-
     public void setStreet(String street) {
         this.street.set(street);
+    }
+
+    public StringProperty streetProperty() {
+        return street;
     }
 
     public int getPostalCode() {
         return postalCode.get();
     }
 
-    public IntegerProperty postalCodeProperty() {
-        return postalCode;
-    }
-
     public void setPostalCode(int postalCode) {
         this.postalCode.set(postalCode);
+    }
+
+    public IntegerProperty postalCodeProperty() {
+        return postalCode;
     }
 
     public String getCity() {
         return city.get();
     }
 
-    public StringProperty cityProperty() {
-        return city;
-    }
-
     public void setCity(String city) {
         this.city.set(city);
+    }
+
+    public StringProperty cityProperty() {
+        return city;
     }
 
     public LocalDate getBirthday() {
         return birthday.get();
     }
 
-    public ObjectProperty<LocalDate> birthdayProperty() {
-        return birthday;
-    }
-
     public void setBirthday(LocalDate birthday) {
         this.birthday.set(birthday);
+    }
+
+    public ObjectProperty<LocalDate> birthdayProperty() {
+        return birthday;
     }
 }
--- src/adress/view/PersonOverviewController.java
+++ src/adress/view/PersonOverviewController.java
@@ -5,12 +5,13 @@ package adress.view;
  */
 
 
+import adress.MainApp;
+import adress.model.Person;
+import adress.util.DateUtil;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import adress.MainApp;
-import adress.model.Person;
 
 public class PersonOverviewController {
     @FXML
@@ -35,17 +36,43 @@ public class PersonOverviewController {
 
     private MainApp mainApp;
 
-    public PersonOverviewController(){
+    public PersonOverviewController() {
     }
 
     @FXML
-    private void initialize(){
-        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
-        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+    private void initialize() {
+        firstNameColumn.setCellValueFactory(
+                cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(
+                cellData -> cellData.getValue().lastNameProperty());
+
+        showPersonDetails(null);
+
+        personTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showPersonDetails(newValue));
     }
 
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
         personTable.setItems(mainApp.getPersonData());
+    }
+
+    private void showPersonDetails(Person person) {
+        if (person != null) {
+            firstNameLabel.setText(person.getFirstName());
+            lastNameLabel.setText(person.getLastName());
+            streetLabel.setText(person.getStreet());
+            postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+            cityLabel.setText(person.getCity());
+            birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+        } else {
+            //Person is null, remove all text
+            firstNameLabel.setText("");
+            lastNameLabel.setText("");
+            streetLabel.setText("");
+            postalCodeLabel.setText("");
+            cityLabel.setText("");
+            birthdayLabel.setText("");
+        }
     }
 }
